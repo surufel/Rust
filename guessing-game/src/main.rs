531c94ebab1a3bc22d554de3c2bcb5099db63265
surufel <henriquefrancasmm@gmail.com>
2026-03-17 14:05:07 -0300

    Guessing Game

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,40 @@
+use std::io; // Library for input-output
+use rand::RngExt; // From where I've been studying, I should've used Rng, but I failed to do so, reading further, found out RngExt
+use std::cmp::Ordering;
+
+
+fn main() {
+    
+    println!("Guess your number!");
+    
+    let secret_number = rand::rng().random_range(1..=100);
+    
+    println!("Debug for rngExt: secret number is {}", secret_number);
+
+    loop {
+    
+    println!("Type your guess: ");
+    
+    let mut guess = String::new(); // Declaration of a mutable variable (it is immutable by default)
+    
+    io::stdin()
+        .read_line(&mut guess) // OK
+        .expect("Failed to read input"); // Err
+    println!("Your guess: {}", guess);
+
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue, // The underline "_" means any value 
+        // parse guess string -> u32 (int 32bits)
+    };
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        },
+    }
+    }
+}
